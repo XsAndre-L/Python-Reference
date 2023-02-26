fa4f98dbb6e982eba448cd3fae6581d1876ce881
XsAndre-L <ahloubser12@gmail.com>
2023-02-26 20:54:02 +0200

    commit changes

--- PythonBaseProject/app.py
+++ PythonBaseProject/app.py
@@ -1,4 +1,5 @@
 import pyautogui as pag
+import time
 
 
 def press(imagePath):
@@ -15,6 +16,16 @@ def openApp(appName):
   pag.press('enter')
   
   
+def switchDesktop(next):
+  if next:
+    pag.hotkey('win', 'ctrl', 'right')
+  else:
+    pag.hotkey('win', 'ctrl', 'left')
+    
+    
+  
+  
+  
 # press('./Assets/vscode_readme.png')
 
 # pag.screenshot('sc_shot.png')
@@ -24,10 +35,20 @@ def openApp(appName):
 # press('./Assets/2.png')
 # press('./Assets/1.png')
 
-openApp('vis')
+pag.PAUSE = 1
 openApp('docker')
+openApp('vis')
+
+time.sleep(5)
+
+switchDesktop(True)
+
+openApp('slack')
+openApp('obsidian')
+openApp('chrome')
+press('./Assets/chrome_candide.png')
 
 # print(pag.size())
 
 
-print(pag.position())
+# print(pag.position())
--- README.md
+++ README.md
@@ -9,3 +9,4 @@ ctrl + shift + P -> Python: Create Environment
 ## Libraries
 
 pyautogui -> https://pyautogui.readthedocs.io/en/latest/keyboard.html
+opencv -> https://pypi.org/project/opencv-python/
